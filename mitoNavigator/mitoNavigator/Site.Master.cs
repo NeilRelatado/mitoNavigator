2344628fa6c71a093a9e80eabc5fe25435b88672
NeilRelatado <neilrelatado@live.com>
2017-01-31 12:45:22 -0500

    masta page

---
+++ mitoNavigator/mitoNavigator/Site.Master.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace mitoNavigator
+{
+    public partial class Site : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //logo.ImageMap = 
+            //logo.bitmap=Properties.Resources.Mito_Canada_logo_tag_web2;
+        }
+
+
+    }
+}
---
+++ mitoNavigator/mitoNavigator/home.aspx
@@ -0,0 +1,11 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="home.aspx.cs" Inherits="mitoNavigator.home" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="Body" runat="server">
+
+    <p>
+    home hj</p>
+
+</asp:Content>
